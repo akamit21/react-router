32b0c9b63b048e69723e4919e803a75f77b55ce0
Amit Kumar <akamit21@outlook.com>
2020-01-03 23:50:45 +0530

    File Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./App.css";
+import { Link, Route } from "react-router-dom";
+import Writers from "./components/Writers";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      writers: []
+    };
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:2000/writers")
+      .then(res => res.json())
+      .then(writers => this.setState({ writers }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link to="/writers">Writers</Link>
+          </li>
+        </ul>
+        <Route exact path="/" render={() => <div>kljkjh</div>} />
+        <Route
+          path="/writers"
+          render={props => <Writers {...props} writers={this.state.writers} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
