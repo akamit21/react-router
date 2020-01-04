5a0d645f4cf3808382e9d97026600ed30fdf57b3
Amit Kumar <akamit21@outlook.com>
2020-01-04 21:41:24 +0530

    Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+        perferendis, nesciunt dolores iure repellendus ratione, maxime, facere
+        reprehenderit id veniam ex cum delectus alias voluptatum nam
+        exercitationem odio at vero.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
