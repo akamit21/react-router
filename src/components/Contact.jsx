e00f8c6bd2a65e023d342788d335edca4e1da418
Amit Kumar <akamit21@outlook.com>
2020-01-04 21:41:33 +0530

    Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+        perferendis, nesciunt dolores iure repellendus ratione, maxime, facere
+        reprehenderit id veniam ex cum delectus alias voluptatum nam
+        exercitationem odio at vero.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
