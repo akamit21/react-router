a677287393364fbfeaaaad26d31fb89135581b4a
Amit Kumar <akamit21@outlook.com>
2020-01-05 22:26:29 +0530

    posts menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,9 @@ const Navbar = () => {
             <Link to="/about">About</Link>
           </li>
           <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
