c13d1a145caf0ed6f26aefc6b0244d3e098ad03f
Amit Kumar <akamit21@outlook.com>
2020-01-05 22:26:03 +0530

    Switch added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import About from "../components/About";
 import Contact from "../components/Contact";
+import Posts from "../components/Posts";
+import Post from "../components/Post";
 
 const Routes = () => {
   return (
-    <React.Fragment>
+    <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/about" exact component={About} />
-      <Route path="/contact" exact component={Contact} />
-    </React.Fragment>
+      <Route path="/about" component={About} />
+      <Route path="/posts" exact component={Posts} />
+      <Route path="/posts/:post_id" component={Post} />
+      <Route path="/contact" component={Contact} />
+      <Route path="" />
+    </Switch>
   );
 };
 
